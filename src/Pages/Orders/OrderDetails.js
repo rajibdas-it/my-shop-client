94d2e2e975b43664eb4967b38248d4daa51189c2
rajibdas-it <rajibrad@gmail.com>
2022-11-05 17:50:39 +0600

    done from office

---
+++ src/Pages/Orders/OrderDetails.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const OrderDetails = ({ order, index }) => {
+  const { _id, customerName, phone, prodId, productName, price, status } =
+    order;
+  const [orderProduct, setOrderProduct] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/product/${prodId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrderProduct(data);
+      });
+  }, [prodId]);
+
+  return (
+    <tr>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div>
+            <div className="font-bold">{index + 1}</div>
+          </div>
+        </div>
+      </td>
+      <td className="flex gap-3">
+        <div className="avatar">
+          <div className="mask mask-squircle w-12">
+            <img src={orderProduct?.img} alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+        <div>
+          {productName}
+          <br />
+          <span className="badge badge-ghost badge-sm">Price: ${price}</span>
+        </div>
+      </td>
+      <td>{status}</td>
+      <th className="flex gap-2">
+        <button className="btn btn-outline btn-success">Confirm</button>
+        <button className="btn btn-error">Delete</button>
+      </th>
+    </tr>
+  );
+};
+
+export default OrderDetails;
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../Context/UserContext";
+import OrderDetails from "./OrderDetails";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
-  const currentUser = {
-    email: user.email,
-  };
-  console.log(currentUser);
+  const [storedOrders, setStoredOrders] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/orders?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setStoredOrders(data));
+  }, [user?.email]);
+
   return (
     <div className="w-[80%] mx-auto">
       <div className="text-2xl text-center my-5">
@@ -18,47 +25,20 @@ const Orders = () => {
           <table className="table w-full">
             <thead>
               <tr>
-                <th>Customer Name</th>
+                <th>SL No.</th>
                 <th>Product Details</th>
                 <th>Status</th>
                 <th>Action</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td className="flex gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <img
-                        src="/tailwind-css-component-profile-2@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    Zemlak, Daniel and Leannon
-                    <br />
-                    <span className="badge badge-ghost badge-sm">
-                      Desktop Support Technician
-                    </span>
-                  </div>
-                </td>
-                <td>Purple</td>
-                <th className="flex gap-2">
-                  <button className="btn btn-outline btn-success">
-                    Confirm
-                  </button>
-                  <button className="btn btn-error">Delete</button>
-                </th>
-              </tr>
+              {storedOrders.map((order, index) => (
+                <OrderDetails
+                  key={order._id}
+                  order={order}
+                  index={index}
+                ></OrderDetails>
+              ))}
             </tbody>
           </table>
         </div>
